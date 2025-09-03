7ccbfbe33913118ab1f240eacdbc726a7a371486
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-03 13:45:39 -0500

    mejorar el componente SideBarPacient al agregar animaciones a los enlaces de navegación utilizando framer-motion, y ajustar el estilo visual con bordes redondeados y sombras

--- src/features/paciente/common/components/SideBarPacient.tsx
+++ src/features/paciente/common/components/SideBarPacient.tsx
@@ -1,48 +1,50 @@
+"use client";
 import { BoxesIcon, EditIcon, FileUser } from "lucide-react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const SideBarPacient = () => {
   return (
-    <div className="flex-1 max-w-3xs m h-screen bg-gray-200 p-4 flex flex-col gap-4">
+    <div className="flex-1 max-w-3xs rounded-2xl m-5 shadow-lg bg-gray-100 p-4 flex flex-col gap-4">
       <div className="flex flex-col gap-4">
-        <Link
-          className="font-bold hover:bg-gray-300 p-2 rounded"
-          href={"/pacientes"}
-        >
-          <div className="flex gap-2">
+        <Link href={"/pacientes"}>
+          <motion.div
+            className="font-bold hover:bg-gray-300 p-2 rounded flex gap-2"
+            whileTap={{ scale: 0.9 }}
+          >
             <div>Datos generales</div>
             <BoxesIcon className="w-5 h-5" />
-          </div>
+          </motion.div>
         </Link>
 
-        <Link
-          className="font-bold hover:bg-gray-300 p-2 rounded"
-          href={"/pacientes/registro-clinico"}
-        >
-          <div className="flex gap-2">
-            <div>Registros clinicos</div>
+        <Link href={"/pacientes/registro-clinico"}>
+          <motion.div
+            className="font-bold hover:bg-gray-300 p-2 rounded flex gap-2"
+            whileTap={{ scale: 0.9 }}
+          >
+            <div>Registro clinico</div>
             <EditIcon className="w-5 h-5" />
-          </div>
+          </motion.div>
         </Link>
 
-        <Link
-          className="font-bold hover:bg-gray-300 p-2 rounded"
-          href={"/pacientes/odontograma"}
-        >
-          <div className="flex gap-2">
+        <Link href={"/pacientes/odontograma"}>
+          <motion.div
+            className="font-bold hover:bg-gray-300 p-2 rounded flex gap-2"
+            whileTap={{ scale: 0.9 }}
+          >
             <div>Odontograma</div>
             <FileUser className="w-5 h-5" />
-          </div>
+          </motion.div>
         </Link>
 
-        <Link
-          className="font-bold hover:bg-gray-300 p-2 rounded"
-          href={"/pacientes/periodontograma"}
-        >
-          <div className="flex gap-2">
+        <Link href={"/pacientes/periodontograma"}>
+          <motion.div
+            className="font-bold hover:bg-gray-300 p-2 rounded flex gap-2"
+            whileTap={{ scale: 0.9 }}
+          >
             <div>Periodontograma</div>
             <FileUser className="w-5 h-5" />
-          </div>
+          </motion.div>
         </Link>
       </div>
     </div>
