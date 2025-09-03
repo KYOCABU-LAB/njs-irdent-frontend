cb010cffa3307077b05562896935fbb0c791aeb4
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-03 12:45:27 -0500

    add route link next js component

--- src/features/paciente/common/components/SideBarPacient.tsx
+++ src/features/paciente/common/components/SideBarPacient.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const SideBarPacient = () => {
   return (
     <div className="flex-1 max-w-3xs m h-screen bg-gray-200 p-4 flex flex-col gap-4">
       <ul className="flex flex-col gap-4">
         <li>
-          <a href="#">Datos generales</a>
+          <Link href={"/pacientes"}>Datos generales</Link>
         </li>
         <li>
-          <a href="#">Registros clinicos</a>
+          <Link href={"/pacientes/registro-clinico"}>Registros clinicos</Link>
         </li>
       </ul>
     </div>
