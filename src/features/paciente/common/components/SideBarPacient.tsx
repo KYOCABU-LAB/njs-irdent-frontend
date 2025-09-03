758f18ba5a094abb56a99323db3a8bf25f4f3cbe
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-03 13:34:11 -0500

    refactorizar el componente SideBarPacient para utilizar enlaces directos en lugar de listas, mejorando la estructura y la accesibilidad del menú de navegación

--- src/features/paciente/common/components/SideBarPacient.tsx
+++ src/features/paciente/common/components/SideBarPacient.tsx
@@ -3,20 +3,35 @@ import Link from "next/link";
 const SideBarPacient = () => {
   return (
     <div className="flex-1 max-w-3xs m h-screen bg-gray-200 p-4 flex flex-col gap-4">
-      <ul className="flex flex-col gap-4">
-        <li className="font-bold hover:bg-gray-300 p-2 rounded transition-colors">
-          <Link href={"/pacientes"}>Datos generales</Link>
-        </li>
-        <li className="font-bold hover:bg-gray-300 p-2 rounded">
-          <Link href={"/pacientes/registro-clinico"}>Registros clinicos</Link>
-        </li>
-        <li className="font-bold hover:bg-gray-300 p-2 rounded">
-          <Link href={"/pacientes/odontograma"}>Odontograma</Link>
-        </li>
-        <li className="font-bold hover:bg-gray-300 p-2 rounded">
-          <Link href={"/pacientes/periodontograma"}>Periodontograma</Link>
-        </li>
-      </ul>
+      <div className="flex flex-col gap-4">
+        <Link
+          className="font-bold hover:bg-gray-300 p-2 rounded"
+          href={"/pacientes"}
+        >
+          <div>Datos generales</div>
+        </Link>
+
+        <Link
+          className="font-bold hover:bg-gray-300 p-2 rounded"
+          href={"/pacientes/registro-clinico"}
+        >
+          <div>Registros clinicos</div>
+        </Link>
+
+        <Link
+          className="font-bold hover:bg-gray-300 p-2 rounded"
+          href={"/pacientes/odontograma"}
+        >
+          <div>Odontograma</div>
+        </Link>
+
+        <Link
+          className="font-bold hover:bg-gray-300 p-2 rounded"
+          href={"/pacientes/periodontograma"}
+        >
+          <div>Periodontograma</div>
+        </Link>
+      </div>
     </div>
   );
 };
