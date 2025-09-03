205c4304fb20ffe7ec533af490389b5953de3635
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-03 12:48:58 -0500

    desind basic

--- src/features/paciente/common/components/SideBarPacient.tsx
+++ src/features/paciente/common/components/SideBarPacient.tsx
@@ -4,10 +4,10 @@ const SideBarPacient = () => {
   return (
     <div className="flex-1 max-w-3xs m h-screen bg-gray-200 p-4 flex flex-col gap-4">
       <ul className="flex flex-col gap-4">
-        <li>
+        <li className="font-bold hover:bg-gray-300 p-2 rounded transition-colors">
           <Link href={"/pacientes"}>Datos generales</Link>
         </li>
-        <li>
+        <li className="font-bold hover:bg-gray-300 p-2 rounded">
           <Link href={"/pacientes/registro-clinico"}>Registros clinicos</Link>
         </li>
       </ul>
