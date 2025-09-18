31552a8d3d1cb916ba2abcce6df03035023d74d2
Russel-Fs <russelfloressolano900@gmail.com>
2025-09-18 13:32:21 -0500

    refactor(layout): remove SessionProvider and adjust main content styling
    Remove SessionProvider wrapper as it's no longer needed and adjust main content layout to use flex-col and add padding for better spacing

--- src/shared/components/layouts/DashboardLayout.tsx
+++ src/shared/components/layouts/DashboardLayout.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { NavContext } from "@/shared/context/useNavContext";
 import SideBarDesktop, { SideBarMobile } from "../sidebar/SideBar";
-import { SessionProvider } from "next-auth/react";
+
 import { DashboardHeader } from "../DashboardHeader";
 
 const DashboardLayout = ({
@@ -14,24 +14,22 @@ const DashboardLayout = ({
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <SessionProvider>
-      <NavContext.Provider
-        value={{
-          isNavOpen,
-          toggleNav: () => setIsNavOpen(!isNavOpen),
-          onCloseNav: () => setIsNavOpen(false),
-        }}
-      >
-        <div className="flex h-screen overflow-hidden">
-          <SideBarDesktop />
-          <SideBarMobile />
-          <main className="flex-1 overflow-auto">
-            <DashboardHeader />
-            <div className="min-h-full">{children}</div>
-          </main>
-        </div>
-      </NavContext.Provider>
-    </SessionProvider>
+    <NavContext.Provider
+      value={{
+        isNavOpen,
+        toggleNav: () => setIsNavOpen(!isNavOpen),
+        onCloseNav: () => setIsNavOpen(false),
+      }}
+    >
+      <div className="flex h-screen overflow-hidden">
+        <SideBarDesktop />
+        <SideBarMobile />
+        <main className="flex-1 overflow-auto flex flex-col">
+          <DashboardHeader />
+          <div className="flex-1 p-6">{children}</div>
+        </main>
+      </div>
+    </NavContext.Provider>
   );
 };
 
